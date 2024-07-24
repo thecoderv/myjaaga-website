d9db2d87a05ecac439befa6224872f210788c980
vineemurali <121651746+vineemurali@users.noreply.github.com>
2024-07-24 17:57:45 -0500

    feature : Function to protect routes. Only Logged in user can access

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
 import * as auth from "../controllers/auth.js";
+import { requireSignin } from "../middlewares/auth.js";
 
-router.get("/auth", auth.welcome);
+router.get("/auth", requireSignin, auth.welcome);
 router.post("/pre-register", auth.preRegister);
 router.post("/register", auth.register);
 router.post("/login", auth.login);
